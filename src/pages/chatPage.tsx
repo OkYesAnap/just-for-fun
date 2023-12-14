652b4f03e5d57d36722fde8ff4d40afaaeee67db
Panaseiko Andrii <andrii.panaseiko@akvelon.com>
2023-12-14 21:21:33 +0200

    Ask button styles

--- src/pages/chatPage.tsx
+++ src/pages/chatPage.tsx
@@ -47,6 +47,18 @@ const InputBlock = styled.div`
 
 const ButtonAskBlock = styled.button`
   margin-bottom: 0.5rem;
+  padding: 1rem 2rem;
+  background-color: #2196F3;
+  color: white;
+  font-size: 1rem;
+  border: none;
+  border-radius: 0.25rem;
+  cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'};
+  opacity: ${props => props.disabled ? '0.5' : '1'};
+
+  &:hover {
+    background-color: #1976D2;
+  }
 `
 
 function ChatPage() {
