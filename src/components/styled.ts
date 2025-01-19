78a94b752d2249c0ea57c4f5c0f515222eb59669
Panaseiko Andrii <andrii.panaseiko@akvelon.com>
2025-01-19 12:05:53 +0200

    Added some css fixes

--- src/components/styled.ts
+++ src/components/styled.ts
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const ButtonAsk = styled.button`
-  margin: 0.1rem;
-  padding: 0.5rem 0.2rem;
+  margin: 0.3rem 0.1rem;
+  padding: 1rem 0.75rem;
   background-color: #2196F3;
   color: white;
-  font-size: 1rem;
+  font-size: 1.3rem;
   border: 1px solid white;
   border-radius: 0.25rem;
   cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'};
