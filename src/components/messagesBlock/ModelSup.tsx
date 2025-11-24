ad0d7f03e6f53eb655c9eebe07a51a01d84df8a3
okyesnap <okyesanap@gmail.com>
2025-11-24 23:10:32 +0200

    Add dynamic timer.

--- src/components/messagesBlock/ModelSup.tsx
+++ src/components/messagesBlock/ModelSup.tsx
@@ -1,4 +1,5 @@
-import {IEngineMessage} from "../../api/gptApi";
+import {EngineRole, IEngineMessage} from "../../api/gptApi";
+import {useEffect, useRef, useState} from "react";
 
 interface ModelSupProps {
     message: IEngineMessage;
@@ -11,7 +12,28 @@ const supStyle = {
 };
 
 const ModelSup: React.FC<ModelSupProps> = ({message}) => {
-    return <sup style={supStyle}>{message.model} {message.time && (message.time / 1000 + " Sec ")}</sup>
+    const startTimer = useRef<number>(Date.now());
+    const [currentTime, setCurrentTime] = useState<number | undefined>(message.time);
+    const timer = useRef<ReturnType<typeof setInterval> | null>(null);
+
+    useEffect(() => {
+        if (message.time) {
+            setCurrentTime(message.time);
+        }
+        if (message.role === EngineRole.inprogress) {
+            timer.current = setInterval(() => {
+                setCurrentTime(Date.now() - (startTimer.current));
+            }, 75)
+        }
+        return () => {
+            if (timer.current) {
+                clearInterval(timer.current);
+            }
+        };
+    }, [message.role, message.time]);
+
+    return <sup
+        style={supStyle}>{message.model} {currentTime && ((currentTime / 1000).toFixed(2) + " Sec ")}</sup>
 };
 
 export default ModelSup;
