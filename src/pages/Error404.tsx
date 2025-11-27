41709b311b2b3978fd3f7e5bde2d70a339d9d6ca
Andrii Panaseiko <okyesanap@gmail.com>
2025-11-27 14:03:01 +0200

    Rename Page.

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {chatPages} from "../api/params";
 
+const firstPage = Object.keys(chatPages)[0];
 const Error404 = () => (
     <div>
         <h1>Not Found 404</h1>
         <h2>
-            <Link className="App-link" to="/">
-                Go to main
+            <Link className="App-link" to={`/${firstPage}`}>
+                Go to {firstPage}
             </Link>
         </h2>
     </div>
