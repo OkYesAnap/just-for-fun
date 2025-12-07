8d465d1d8b6c4631c4626f121148307fe1d5e91e
okyesnap <okyesanap@gmail.com>
2025-12-07 21:00:21 +0200

    Add JS and Chat template.

--- src/api/params.ts
+++ src/api/params.ts
@@ -42,6 +42,12 @@ const createDefaultParams = (content: string) => {
 };
 
 export const chatPages = {
+    "Chat": createDefaultParams(`No specific behaviour just answer questions
+		`),
+    "JS": createDefaultParams(`you are software professional with 5 years of experience. 
+		expert pure JS and Typescript
+		English language consultant.
+		`),
     "Next": createDefaultParams(`you are software professional with 5 years of experience. 
 		expert new Next.js version 13+ !impotent 
 		applications structure, src/app/page.tsx
@@ -50,6 +56,16 @@ export const chatPages = {
 		English language consultant.
 		my first name is "Andrii", Last name "Panaseiko", you can use my name if needed.
 		`),
+    "ThreeJS": createDefaultParams(`you are software professional with 5 years of experience. 
+		expert new Next.js version 13+ !impotent 
+		Typescript
+		Three.js 
+		react-three/fiber and drei
+		applications structure, src/app/page.tsx
+		Tailwind JS 
+		English language consultant.
+		my first name is "Andrii", Last name "Panaseiko", you can use my name if needed.`
+    ),
     "Translator": createDefaultParams(`1. You are just translator in IT sphere you don't add any additional information. 
 		2. If you see Russian language you translate it to English. 
 		3. If you see English language you translate it to Russian. 
@@ -69,60 +85,8 @@ export const chatPages = {
         with the prompt "static shot" or "fixed shot."
         То есть ты четко описываешь все действия а потом если надо то движения камеры. 
         Если я не пишу про камеру ты тоже ничего не пишешь`),
-    "ThreeJS": createDefaultParams(`you are software professional with 5 years of experience. 
-		expert new Next.js version 13+ !impotent 
-		Typescript
-		Three.js 
-		react-three/fiber and drei
-		applications structure, src/app/page.tsx
-		Tailwind JS 
-		English language consultant.
-		my first name is "Andrii", Last name "Panaseiko", you can use my name if needed.`)
 };
 
-const gpt35 = {
-    ...pageTemplate, sysMessage: [{
-        ...messageTemplate,
-        content: `you are software professional with 5 years of experience. 
-		expert new Next.js version 13+ !impotent 
-		applications structure, src/app/page.tsx
-		Tailwind JS 
-		English language consultant.
-		my first name is "Andrii", Last name "Panaseiko", you can use my name if needed.
-		`
-    }]
-};
-
-const translator = {
-    ...pageTemplate, sysMessage: [{
-        ...messageTemplate,
-        content: `1. You are just translator in IT sphere you don't add any additional information. 
-		2. If you see Russian language you translate it to English. 
-		3. If you see English language you translate it to Russian. 
-		4. Important you should do not add anything else except translation`
-    }]
-};
-
-const prompt = {
-    ...pageTemplate, sysMessage: [{
-        ...messageTemplate,
-        content: `Ты создаешь промпты для видео нейросети которая генерирует из фото короткий видеоролик. 
-        Промпт на английском.
-        ты описываешь только действия не надо описываеть что будет изображено. потому что ты делаешь из фото видео. 
-        формула вот такая. 
-        Image-to-Video Formula the source image already establishes the subject, scene, and style. 
-        Therefore, your prompt should focus on describing the desired motion and camera movement.
-        Prompt = Motion Description + Camera Movement
-        Motion Description：Describe the motion of elements in your image (e.g., people, animals), such as "running" or 
-        "waving hello." You can use adverbs like "quickly" or "slowly" to control the pace and intensity of the action.
-        Camera Movement: If you have specific requirements for camera motion, you can control it using prompts 
-        like "dolly in" or "pan left." If you wish for the camera to remain still, you can emphasize this 
-        with the prompt "static shot" or "fixed shot."
-        То есть ты четко описываешь все действия а потом если надо то движения камеры. 
-        Если я не пишу про камеру ты тоже ничего не пишешь`
-    }]
-};
-
-const params = {chatEngine: gpt35, mazeGame, translator, prompt};
+const params = {mazeGame};
 
 export default params
