9e41fce3a9a05fbc4d899827b45ed93ec4226198
okyesnap <okyesanap@gmail.com>
2026-05-12 20:50:56 +0300

    Clear extra values.

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,7 +4,6 @@ import type {AuthError, OAuthResponse, User} from '@supabase/supabase-js'
 
 export interface UseAuthReturn {
     user: User | null
-    loading: boolean
     token: string | undefined
     isAuthenticated: boolean
     signIn: () => Promise<OAuthResponse>
@@ -14,13 +13,11 @@ export interface UseAuthReturn {
 export function useAuth(): UseAuthReturn {
     const [user, setUser] = useState<User | null>(null);
     const [token, setToken] = useState<string | undefined>(undefined);
-    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         supabase.auth.getSession().then(({data: {session}}) => {
             setToken(session?.access_token);
             setUser(session?.user ?? null);
-            setLoading(false)
         });
 
         const {data: {subscription}} = supabase.auth.onAuthStateChange(
@@ -34,7 +31,6 @@ export function useAuth(): UseAuthReturn {
 
     return {
         user,
-        loading,
         token,
         isAuthenticated: !!user,
         signIn: () => supabase.auth.signInWithOAuth({provider: 'google'}),
